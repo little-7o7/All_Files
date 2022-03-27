8f040660b5ae7c09db4c2c98c08ba3307770aaab
Makhmudkhon Muniskhonov <97856162+Maxmudxon@users.noreply.github.com>
2022-03-27 16:45:46 +0500

    asdf

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className = "App">
-      
-    </div>
-  );
+	const [likes, setLikes] = useState(0);
+	const [value, setValue] = useState("Text in input");
+
+	function add() {
+		setLikes(likes + 1);
+	}
+
+	function get() {
+		setLikes(likes - 1);
+	}
+
+	return (
+		<div className="App">
+			<h1>{likes}</h1>
+			<h1>{value}</h1>
+			<input
+				type="text"
+				value={value}
+				onChange={(event) => setValue(event.target.value)}
+			></input>
+			<br></br>
+			<button onClick={add}>Add</button>
+			<button onClick={get}>Get</button>
+		</div>
+	);
 }
 
 export default App;
