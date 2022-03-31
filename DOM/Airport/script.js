26889a68d4ffc3f8e61cf8d8fbb1ac9654e3cb0a
Makhmudkhon Muniskhonov <97856162+Maxmudxon@users.noreply.github.com>
2022-03-31 20:29:12 +0500

    asdf

---
+++ DOM/Airport/script.js
@@ -0,0 +1,92 @@
+// let airways_7o7 = [
+//     {
+//         id: 1,
+//         name: 'Thai Airways',
+//         country: 'Thailand',
+//         logo: 'https://upload.wikimedia.org/wikipedia/en/thumb/5/58/Thai_Airways_Logo.svg/200px-Thai_Airways_Logo.svg.png',
+//         slogan: 'Smooth as Silk / I Fly THAI',
+//         head_quaters: 'Jom Phol Subdistrict, Chatuchak, Bangkok, Thailand',
+//         website: 'www.thaiairways.com',
+//         established: '1960',
+//     },
+//     {
+//         id: 2,
+//         name: 'Swiss International Air Lines',
+//         country: 'Switzerland',
+//         logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Swiss_International_Air_Lines_Logo_2011.svg/200px-Swiss_International_Air_Lines_Logo_2011.svg.png',
+//         slogan: 'Made of Switzerland. The Airline of Switzerland',
+//         head_quaters: 'EuroAirport Basel Mulhouse Freiburg near Basel, Switzerland',
+//         website: 'www.swiss.com',
+//         established: '2002',
+//     },
+//     {
+//         id: 3,
+//         name: 'Swiss International Air Lines',
+//         country: 'Switzerland',
+//         logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Swiss_International_Air_Lines_Logo_2011.svg/200px-Swiss_International_Air_Lines_Logo_2011.svg.png',
+//         slogan: 'Made of Switzerland. The Airline of Switzerland',
+//         head_quaters: 'EuroAirport Basel Mulhouse Freiburg near Basel, Switzerland',
+//         website: 'www.swiss.com',
+//         established: '2002',
+//     },
+//     {
+//         id: 4,
+//         name: 'Swiss International Air Lines',
+//         country: 'Switzerland',
+//         logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Swiss_International_Air_Lines_Logo_2011.svg/200px-Swiss_International_Air_Lines_Logo_2011.svg.png',
+//         slogan: 'Made of Switzerland. The Airline of Switzerland',
+//         head_quaters: 'EuroAirport Basel Mulhouse Freiburg near Basel, Switzerland',
+//         website: 'www.swiss.com',
+//         established: '2002',
+//     },
+//     {
+//         id: 5,
+//         name: 'Swiss International Air Lines',
+//         country: 'Switzerland',
+//         logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/Swiss_International_Air_Lines_Logo_2011.svg/200px-Swiss_International_Air_Lines_Logo_2011.svg.png',
+//         slogan: 'Made of Switzerland. The Airline of Switzerland',
+//         head_quaters: 'EuroAirport Basel Mulhouse Freiburg near Basel, Switzerland',
+//         website: 'www.swiss.com',
+//         established: '2002',
+//     },
+// ];
+
+
+// if (localStorage.getItem('airways_7o7') == null) {
+//     setStorage(airways_7o7)
+// }
+
+// let text = localStorage.getItem("airways_7o7");
+// airways_7o7 = JSON.parse(text);
+
+// function setStorage(arr) {
+//     let airways_7o7JSON = JSON.stringify(arr)
+//     localStorage.setItem("airways_7o7", airways_7o7JSON);
+//     console.log(arr);
+// }
+
+// let body = document.querySelector('body')
+// let header = body.querySelector('header')
+
+// let add_btn_for_modal = header.querySelector('.add_btn')
+
+// let filter_icon = header.querySelector('.filter_icon')
+// let search_bar = header.querySelector('.search_bar')
+// let select = search_bar.querySelector('select')
+// let input = search_bar.querySelector('input')
+
+// let search_for_name = header.querySelector('.search_for_name')
+
+// let main = body.querySelector('main')
+
+// let white_back = body.querySelector('.white_back')
+// let change_delete = body.querySelector('.change_delete')
+
+// let blur_modal = body.querySelector('blur_modal')
+// let screen = body.querySelector('screen')
+
+// add_btn_for_modal.onclick = () => {
+
+// }
+
+
