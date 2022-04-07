23406d6b630adbe4919b834ceb7c4164802d72b0
Makhmudkhon Muniskhonov <97856162+Maxmudxon@users.noreply.github.com>
2022-04-07 17:30:54 +0500

    1 exam

---
+++ DOM/1_exam/script.js
@@ -0,0 +1,12 @@
+let body = document.querySelector('body')
+let conteiner = body.querySelector('.conteiner')
+
+let header = conteiner.querySelector('header')
+let search_panel = conteiner.querySelector('.search-panel')
+let ul = conteiner.querySelector('ul')
+let app_add_form = conteiner.querySelector('.app-add-form')
+
+console.log(header);
+console.log(search_panel);
+console.log(ul);
+console.log(app_add_form);
