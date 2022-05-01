4eef4264d125f6b3b129c815a3fb8d346c382f43
unknown <munisxonovmaxmudxon@gmail.com>
2022-05-01 10:16:22 +0500

    asdf

---
+++ DOM/myTelegram/assets/js/resize.js
@@ -0,0 +1,47 @@
+let size = '480px'
+
+if (localStorage.getItem('__little_7o7__myTelegram_resize_size') == null) {
+    setStorage(size)
+} else {
+    size = localStorage.getItem("__little_7o7__myTelegram_resize_size");
+}
+
+function setStorage(px) {
+    localStorage.setItem("__little_7o7__myTelegram_resize_size", px);
+    console.log(px);
+}
+
+let dragging = 0,
+    body = document.body,
+    target = document.getElementById('dragbar');
+
+body.style.setProperty("--left-width", size);
+
+function clearJSEvents() {
+    dragging = 0;
+    body.removeEventListener("mousemove", resize);
+    body.classList.remove('resizing');
+}
+
+function resize(e) {
+    if (e.pageX > 635 || e.pageX < 200) {
+        return;
+    }
+    body.style.setProperty("--left-width", e.pageX + 'px');
+
+    console.log(size);
+
+    setStorage(e.pageX + 'px')
+}
+
+target.onmousedown = function (e) {
+    e.preventDefault();
+    dragging = 1;
+    body.addEventListener('mousemove', resize);
+    body.classList.add('resizing');
+};
+
+document.onmouseup = function () {
+    dragging ? clearJSEvents() : '';
+};
+
